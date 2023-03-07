051d76d64c8a49525e2b93155b86404ec90bef4d
pablovisual <pabloarmandosandoval@gmail.com>
2023-03-07 06:04:55 +0000

    coding challenge complete

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,6 +1,6 @@
 
 public class Fibonacci {
-    /**
+    /*
      * Return the nth number of fibonacci sequence.
      * The fibonacci sequence is calculated by adding the previous two numbers in the sequence to obtain the next
      * number. The first two numbers in the whole fibonacci sequence are always 0 and 1.
